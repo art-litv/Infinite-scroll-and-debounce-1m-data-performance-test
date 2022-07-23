f38ee762aa9b0663aa9cdaa2c4cefb1bcc3d1230
art-litv <artem15052003@gmail.com>
2022-07-23 20:12:04 +0300

    upd

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import type { AppProps } from "next/app";
 import { NextUIProvider } from "@nextui-org/react";
-import "react-perfect-scrollbar/dist/css/styles.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
